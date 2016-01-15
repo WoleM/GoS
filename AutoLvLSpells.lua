7c2d935c98c6ae8db34c7a107b51d8b7c95bb4d7
WoleM <WoleM@users.noreply.github.com>
2016-01-15 15:10:01 +0200

    Create AutoLvLSpells.lua

---
+++ AutoLvLSpells.lua
@@ -0,0 +1,261 @@
+local myHero = GetMyHero()
+if GetObjectName(myHero) == "Aatrox" then
+leveltable = {_E,_W,_E,_Q,_E,_R,_E,_W,_E,_W,_R,_W,_W,_Q,_Q,_R,_Q,_Q} 
+elseif GetObjectName(myHero) == "Ahri" then 
+leveltable = {_Q,_E,_W,_Q,_Q,_R,_Q,_W,_Q,_W,_R,_W,_W,_E,_E,_R,_E,_W}
+elseif GetObjectName(myHero) == "Akali" then
+leveltable = {_Q,_W,_Q,_E,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Alistar" then
+leveltable = {_Q,_E,_W,_Q,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Amumu" then
+leveltable = {_W,_E,_Q,_E,_E,_R,_E,_Q,_E,_Q,_R,_Q,_Q,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Anivia" then
+leveltable = {_Q,_E,_E,_W,_E,_R,_E,_Q,_E,_Q,_R,_Q,_Q,_W,_W,_R,_W,_W}	
+elseif GetObjectName(myHero) == "Annie" then
+leveltable = {_Q,_W,_Q,_E,_Q,_R,_Q,_W,_Q,_W,_R,_W,_W,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Ashe" then
+leveltable = {_W,_Q,_E,_W,_W,_R,_W,_Q,_W,_Q,_R,_Q,_Q,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Azir" then
+leveltable = {_W,_Q,_E,_Q,_Q,_R,_Q,_W,_Q,_W,_R,_W,_W,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Bard" then
+leveltable = {_Q,_W,_E,_Q,_Q,_R,_W,_Q,_Q,_W,_R,_W,_W,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Blitzcrank" then
+leveltable = {_Q,_E,_W,_Q,_Q,_R,_W,_Q,_E,_Q,_R,_W,_W,_W,_E,_R,_E,_E}
+elseif  GetObjectName(myHero) == "Brand" then
+leveltable = {_W,_Q,_E,_W,_W,_R,_W,_Q,_W,_Q,_R,_Q,_Q,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Braum" then
+leveltable = {_Q,_E,_W,_Q,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Caitlyn" then
+leveltable = {_Q,_E,_Q,_W,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Cassiopeia" then
+leveltable = {_Q,_E,_E,_W,_E,_R,_E,_Q,_E,_Q,_R,_Q,_Q,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Chogath" then
+leveltable = {_E,_Q,_W,_Q,_W,_R,_E,_W,_W,_W,_R,_Q,_E,_E,_E,_R,_Q,_Q}
+elseif GetObjectName(myHero) == "Corki" then
+leveltable = {_Q,_E,_W,_Q,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Darius" then
+leveltable = {_Q,_W,_E,_Q,_Q,_R,_Q,_W,_Q,_W,_R,_W,_E,_W,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Diana" then
+leveltable = {_Q,_W,_Q,_E,_Q,_R,_Q,_W,_Q,_W,_R,_W,_W,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "DrMundo" then
+leveltable = {_Q,_E,_Q,_W,_Q,_R,_Q,_W,_Q,_W,_R,_Q,_Q,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Draven" then
+leveltable = {_Q,_W,_E,_Q,_Q,_R,_Q,_W,_Q,_W,_R,_W,_W,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Ekko" then
+leveltable = {_Q,_W,_Q,_E,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R,_Q,_Q}
+elseif GetObjectName(myHero) == "Elise" then
+leveltable = {_W,_Q,_E,_Q,_Q,_R,_W,_Q,_Q,_W,_R,_W,_W,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Evelynn" then
+leveltable = {_Q,_E,_W,_Q,_Q,_R,_Q,_E,_Q,_E,_R,_W,_E,_E,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Ezreal" then
+leveltable = {_Q,_E,_Q,_W,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Fiddlesticks" then
+leveltable = {_W,_E,_Q,_W,_W,_R,_W,_Q,_W,_Q,_R,_Q,_Q,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Fiora" then
+leveltable = {_Q,_W,_E,_Q,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Fizz" then
+leveltable = {_E,_W,_Q,_E,_E,_R,_E,_Q,_E,_Q,_R,_Q,_Q,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Galio" then
+leveltable = {_Q,_W,_Q,_E,_Q,_R,_Q,_W,_Q,_E,_R,_W,_W,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Gangplank" then
+leveltable = {_Q,_E,_W,_Q,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Garen" then
+leveltable = {_Q,_E,_E,_W,_E,_R,_E,_Q,_E,_Q,_R,_Q,_Q,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Gnar" then
+leveltable = {_Q,_W,_E,_Q,_Q,_R,_Q,_W,_Q,_W,_R,_W,_W,_E,_E,_R,_W,_W}
+elseif GetObjectName(myHero) == "Gragas" then
+leveltable = {_Q,_E,_W,_E,_E,_R,_E,_W,_E,_W,_R,_W,_W,_Q,_Q,_R,_Q,_Q}
+elseif GetObjectName(myHero) == "Graves" then
+leveltable = {_Q,_E,_Q,_W,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Hecarim" then
+leveltable = {_W,_Q,_E,_Q,_Q,_R,_W,_Q,_Q,_W,_R,_W,_W,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Heimerdinger" then
+leveltable = {_Q,_W,_E,_Q,_Q,_R,_W,_Q,_Q,_W,_R,_W,_W,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Irelia" then
+leveltable = {_E,_Q,_W,_W,_W,_R,_W,_E,_W,_E,_R,_E,_E,_Q,_Q,_R,_Q,_Q}
+elseif GetObjectName(myHero) == "Janna" then
+leveltable = {_E,_Q,_W,_E,_E,_R,_E,_W,_E,_W,_R,_W,_W,_Q,_Q,_R,_Q,_Q}
+elseif GetObjectName(myHero) == "JarvanIV" then
+leveltable = {_E,_Q,_Q,_W,_E,_R,_Q,_Q,_E,_W,_R,_Q,_E,_W,_E,_R,_W,_W}
+elseif GetObjectName(myHero) == "Jax" then
+leveltable = {_E,_Q,_W,_W,_W,_R,_W,_Q,_W,_Q,_R,_Q,_Q,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Jayce" then
+leveltable = {_Q,_W,_E,_Q,_Q,_R,_Q,_W,_Q,_W,_R,_W,_E,_W,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Jinx" then
+leveltable = {_Q,_W,_E,_Q,_Q,_R,_Q,_W,_Q,_W,_R,_W,_W,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Kalista" then
+leveltable = {_W,_E,_Q,_E,_E,_R,_E,_Q,_E,_Q,_R,_Q,_Q,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Karma" then
+leveltable = {_Q,_E,_Q,_W,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Karthus" then
+leveltable = {_Q,_E,_Q,_W,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Kassadin" then
+leveltable = {_Q,_W,_E,_Q,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Katarina" then
+leveltable = {_Q,_E,_W,_Q,_Q,_R,_Q,_W,_Q,_W,_R,_W,_E,_W,_E,_R,_W,_W}
+elseif GetObjectName(myHero) == "Kayle" then
+leveltable = {_E,_Q,_W,_Q,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Kennen" then
+leveltable = {_Q,_E,_W,_Q,_Q,_R,_Q,_W,_Q,_W,_R,_W,_W,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Khazix" then
+leveltable = {_Q,_W,_E,_Q,_Q,_R,_Q,_W,_Q,_W,_R,_W,_W,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Kindred" then
+leveltable = {_W,_Q,_E,_Q,_Q,_R,_Q,_W,_Q,_W,_R,_W,_W,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "KogMaw" then
+leveltable = {_W,_E,_W,_Q,_W,_R,_W,_E,_W,_E,_R,_E,_E,_Q,_Q,_R,_Q,_Q}
+elseif GetObjectName(myHero) == "Leblanc" then
+leveltable = {_Q,_W,_E,_Q,_Q,_R,_Q,_W,_Q,_W,_R,_W,_W,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Leesin" then
+leveltable = {_Q,_E,_W,_Q,_Q,_R,_Q,_W,_Q,_W,_R,_W,_W,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Leona" then
+leveltable = {_E,_Q,_W,_W,_W,_R,_W,_Q,_W,_Q,_R,_Q,_Q,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Lissandra" then
+leveltable = {_Q,_E,_W,_Q,_Q,_R,_Q,_W,_Q,_W,_R,_W,_W,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Lucian" then
+leveltable = {_Q,_E,_Q,_W,_Q,_R,_Q,_W,_Q,_W,_R,_W,_W,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Lulu" then
+leveltable = {_Q,_W,_E,_Q,_Q,_R,_Q,_W,_Q,_W,_R,_W,_W,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Lux" then
+leveltable = {_E,_Q,_E,_W,_E,_R,_E,_Q,_E,_Q,_R,_Q,_Q,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Malphite" then
+leveltable = {_E,_Q,_Q,_W,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Malzahar" then
+leveltable = {_Q,_E,_E,_Q,_W,_R,_E,_E,_Q,_E,_R,_Q,_Q,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Maokai" then
+leveltable = {_E,_Q,_W,_Q,_Q,_R,_W,_Q,_Q,_W,_R,_W,_W,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "MasterYi" then
+leveltable = {_Q,_E,_W,_Q,_Q,_R,_E,_Q,_E,_Q,_R,_E,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "MissFortune" then
+leveltable = {_Q,_W,_E,_Q,_Q,_R,_Q,_W,_Q,_W,_R,_W,_W,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Mordekaiser" then
+leveltable = {_E,_Q,_E,_W,_E,_R,_E,_Q,_E,_Q,_R,_Q,_Q,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Morgana" then
+leveltable = {_Q,_E,_W,_Q,_Q,_Q,_R,_Q,_E,_Q,_R,_E,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Nami" then
+leveltable = {_Q,_W,_E,_W,_W,_R,_W,_E,_W,_Q,_R,_E,_E,_E,_Q,_R,_Q,_Q}
+elseif GetObjectName(myHero) == "Nasus" then
+leveltable = {_Q,_W,_Q,_E,_Q,_R,_Q,_W,_Q,_W,_R,_W,_W,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Nautilus" then
+leveltable = {_E,_Q,_W,_E,_E,_R,_E,_W,_E,_W,_R,_W,_W,_Q,_Q,_R,_Q,_Q}
+elseif GetObjectName(myHero) == "Nidalee" then
+leveltable = {_Q,_E,_W,_Q,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Nocturne" then
+leveltable = {_Q,_W,_E,_Q,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Nunu" then
+leveltable = {_Q,_W,_E,_Q,_E,_R,_E,_E,_E,_W,_R,_W,_W,_W,_Q,_R,_Q,_Q}
+elseif GetObjectName(myHero) == "Olaf" then
+leveltable = {_Q,_W,_E,_Q,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Orianna" then
+leveltable = {_Q,_E,_Q,_W,_Q,_R,_Q,_W,_Q,_W,_R,_W,_W,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Pantheon" then
+leveltable = {_Q,_W,_E,_Q,_Q,_R,_E,_E,_Q,_E,_R,_Q,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Poppy" then
+leveltable = {_E,_W,_Q,_Q,_Q,_R,_Q,_W,_Q,_W,_R,_W,_E,_W,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Quinn" then
+leveltable = {_E,_Q,_W,_Q,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Rammus" then
+leveltable = {_W,_Q,_W,_E,_W,_R,_W,_E,_W,_E,_R,_E,_E,_Q,_Q,_R,_Q,_Q}
+elseif GetObjectName(myHero) == "RekSai" then
+leveltable = {_Q,_W,_E,_Q,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Renekton" then
+leveltable = {_Q,_E,_W,_Q,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Rengar" then
+leveltable = {_Q,_E,_W,_Q,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Riven" then
+leveltable = {_Q,_E,_W,_Q,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Rumble" then
+leveltable = {_Q,_E,_W,_Q,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Ryze" then
+leveltable = {_Q,_W,_E,_Q,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Sejuani" then
+leveltable = {_W,_E,_Q,_W,_W,_R,_W,_E,_W,_E,_R,_E,_E,_Q,_Q,_R,_Q,_Q}
+elseif GetObjectName(myHero) == "Shaco" then
+leveltable = {_W,_E,_Q,_E,_E,_R,_E,_Q,_E,_Q,_R,_Q,_Q,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Shen" then
+leveltable = {_Q,_E,_Q,_W,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Shyvana" then
+leveltable = {_W,_E,_Q,_W,_W,_R,_W,_E,_W,_E,_R,_E,_E,_Q,_Q,_R,_Q,_Q}
+elseif GetObjectName(myHero) == "Singed" then
+leveltable = {_Q,_E,_Q,_E,_Q,_R,_Q,_W,_Q,_E,_R,_E,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Sion" then
+leveltable = {_Q,_W,_Q,_E,_Q,_R,_Q,_W,_Q,_W,_R,_W,_W,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Sivir" then
+leveltable = {_W,_Q,_E,_Q,_Q,_R,_Q,_W,_Q,_W,_R,_W,_E,_Q,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Skarner" then
+leveltable = {_Q,_W,_E,_Q,_Q,_R,_Q,_W,_Q,_W,_R,_W,_W,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Sona" then
+leveltable = {_Q,_W,_Q,_E,_Q,_R,_Q,_W,_Q,_W,_R,_W,_W,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Soraka" then
+leveltable = {_W,_E,_Q,_W,_W,_R,_W,_E,_W,_E,_R,_E,_E,_Q,_Q,_R,_Q,_Q}
+elseif GetObjectName(myHero) == "Swain" then
+leveltable = {_W,_E,_E,_Q,_E,_R,_E,_Q,_E,_Q,_R,_Q,_Q,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Syndra" then
+leveltable = {_Q,_E,_Q,_W,_Q,_R,_Q,_E,_Q,_E,_R,_E,_W,_E,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "TahmKench" then
+leveltable = {_Q,_W,_E,_Q,_Q,_R,_Q,_W,_Q,_W,_R,_W,_E,_W,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Talon" then
+leveltable = {_W,_E,_Q,_W,_W,_R,_W,_Q,_W,_Q,_R,_Q,_Q,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Taric" then
+leveltable = {_E,_Q,_W,_E,_E,_R,_E,_W,_E,_W,_R,_W,_W,_Q,_Q,_R,_Q,_Q}
+elseif GetObjectName(myHero) == "Teemo" then
+leveltable = {_E,_Q,_W,_E,_E,_R,_E,_E,_Q,_Q,_R,_Q,_Q,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Thresh" then
+leveltable = {_E,_Q,_W,_E,_Q,_R,_Q,_Q,_Q,_W,_R,_W,_W,_W,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Tristana" then
+leveltable = {_E,_W,_E,_W,_E,_R,_E,_Q,_E,_Q,_R,_Q,_Q,_Q,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Trundle" then
+leveltable = {_Q,_W,_Q,_E,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Tryndamere" then
+leveltable = {_E,_Q,_W,_Q,_Q,_R,_Q,_W,_Q,_W,_R,_W,_W,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Twistedfate" then
+leveltable = {_W,_Q,_E,_Q,_Q,_R,_Q,_W,_Q,_W,_R,_W,_W,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Twitch" then
+leveltable = {_E,_W,_E,_Q,_E,_R,_E,_Q,_E,_Q,_R,_Q,_Q,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Udyr" then
+leveltable = {_R,_W,_R,_E,_R,_Q,_R,_W,_R,_W,_W,_W,_E,_E,_E,_E,_Q,_Q}
+elseif GetObjectName(myHero) == "Urgot" then
+leveltable = {_Q,_E,_W,_Q,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Varus" then
+leveltable = {_Q,_W,_E,_Q,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Vayne" then
+leveltable = {_Q,_E,_W,_W,_W,_R,_W,_Q,_W,_Q,_R,_Q,_Q,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Veigar" then
+leveltable = {_Q,_E,_W,_Q,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Velkoz" then
+leveltable = {_Q,_E,_W,_Q,_Q,_R,_Q,_W,_Q,_W,_R,_W,_W,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "Vi" then
+leveltable = {_W,_E,_Q,_Q,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Viktor" then
+leveltable = {_Q,_E,_E,_W,_E,_R,_E,_Q,_E,_Q,_R,_Q,_W,_Q,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Vladimir" then
+leveltable = {_Q,_E,_Q,_W,_Q,_R,_Q,_E,_Q,_E,_R,_E,_W,_E,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Volibear" then
+leveltable = {_W,_E,_Q,_W,_W,_R,_W,_E,_W,_E,_R,_E,_E,_Q,_Q,_R,_Q,_Q}
+elseif GetObjectName(myHero) == "Warwick" then
+leveltable = {_W,_Q,_Q,_E,_E,_R,_E,_Q,_Q,_W,_R,_Q,_W,_W,_W,_R,_E,_E}
+elseif GetObjectName(myHero) == "MonkeyKing" then
+leveltable = {_E,_Q,_W,_E,_E,_R,_E,_Q,_E,_Q,_R,_Q,_Q,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Xerath" then
+leveltable = {_Q,_W,_E,_Q,_W,_R,_Q,_W,_Q,_W,_R,_Q,_W,_E,_E,_R,_E,_E}
+elseif GetObjectName(myHero) == "XinZhao" then
+leveltable = {_Q,_E,_E,_W,_E,_R,_E,_Q,_E,_Q,_R,_Q,_Q,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Yasuo" then
+leveltable = {_Q,_E,_Q,_W,_Q,_R,_Q,_E,_Q,_E,_R,_E,_W,_E,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Yorick" then
+leveltable = {_E,_W,_Q,_E,_E,_R,_E,_W,_E,_W,_R,_W,_W,_Q,_Q,_R,_Q,_Q}
+elseif GetObjectName(myHero) == "Zac" then
+leveltable = {_W,_Q,_E,_E,_E,_R,_E,_W,_E,_W,_R,_W,_W,_Q,_Q,_R,_Q,_Q}
+elseif GetObjectName(myHero) == "Zed" then
+leveltable = {_Q,_W,_E,_Q,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Ziggs" then
+leveltable = {_Q,_E,_W,_Q,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Zilean" then
+leveltable = {_Q,_W,_Q,_E,_Q,_R,_Q,_E,_Q,_E,_R,_E,_W,_E,_W,_R,_W,_W}
+elseif GetObjectName(myHero) == "Zyra" then
+leveltable = {_Q,_W,_Q,_E,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R._W,_W}
+end
+
+OnTick(function(myHero)
+LevelSpell(leveltable[GetLevel(myHero)])
+
+end)
